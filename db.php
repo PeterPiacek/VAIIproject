158e9b9c39a95b2ee694e65e0b4f74358c20b77a
Peter <peter.piacek00@gmail.com>
2020-11-22 20:29:27 +0100

    update database

--- db.php
+++ db.php
@@ -1,7 +1,38 @@
 <?php
-
-$con = mysqli_connect("localhost", "root", "", "skola");
-
-function test() {
-	echo "test";
-}
+	class Database {
+		
+		private $con;
+		
+		public function __construct() {
+			$this->con = mysqli_connect("localhost", "root", "", "skola");
+		}
+		
+		public function addActivity($date, $activity, $description) {
+			mysqli_query($this->con, "INSERT INTO plan_aktivit (date, activity, description) VALUES ('$date', '$activity', '$description');");
+		}
+		
+		public function loadActivity() {
+			$plans = mysqli_query($this->con, "SELECT * FROM plan_aktivit ORDER BY date ASC");
+			while ($row = mysqli_fetch_assoc($plans)){
+				echo '<tr>';
+				echo '<td>'.$row['date'].'</td>';
+				echo '<td>'.$row['activity'].'</td>';
+				echo '<td>'.$row['description'].'</td>';
+				echo '<td>
+					<form method="POST">
+						<button type="submit" name="Delete" value='.$row['id'].'>Delete</button>
+						<button type="submit" name="Update_tag" value='.$row['id'].'>Update</button>
+					</form>
+				</td>';
+			}
+		}
+		
+		public function updateActivity($id, $date, $activity, $description) {
+			mysqli_query($this->con, "UPDATE plan_aktivit SET date='$date', activity='$activity', description='$description' WHERE id='$id'");
+		}
+		
+		public function deleteActivity($id) {
+			mysqli_query($this->con, "DELETE FROM plan_aktivit WHERE id=$id");
+		}
+		
+	}
--- plan_aktivit.php
+++ plan_aktivit.php
@@ -1,34 +1,17 @@
 <?php
 	require 'db.php';
-	//prida zaznam do DB
-	function addActivity($con){
-		$date = $_POST['date'];
-		$activity = $_POST['activity'];
-		$description = $_POST['description'];
-		mysqli_query($con, "INSERT INTO plan_aktivit (date, activity, description) VALUES ('$date', '$activity', '$description');");
-	}
-	//vymaze zaznam z DB
-	function deleteActivity($con){
-		$id = $_POST['Delete'];
-		mysqli_query($con, "DELETE FROM plan_aktivit WHERE id=$id");
-	}
-	//upravy zaznam v DB
-	function updateActivity($con) {
-		$id = $_POST['Update'];
-		$date = $_POST['date'];
-		$activity = $_POST['activity'];
-		$description = $_POST['description'];
-		mysqli_query($con, "UPDATE plan_aktivit SET date='$date', activity='$activity', description='$description' WHERE id='$id'");
-	}
+	$database = new Database();
+	
 	//kontroluje aky prikaz sa zadal, a ci ma potrebne data
 	if (isset($_POST['date']) && isset($_POST['activity']) && isset($_POST['description']) && isset($_POST['Add'])){
-		addActivity($con);
+		$database->addActivity($_POST['date'], $_POST['activity'], $_POST['description']);
 	} else if (isset($_POST['Delete'])) {
-		deleteActivity($con);
+		$database->deleteActivity($_POST['Delete']);
 	} else if (isset($_POST['date']) && isset($_POST['activity']) && isset($_POST['description']) && isset($_POST['Update'])) {
-		updateActivity($con);
+		$database->updateActivity($_POST['Update'], $_POST['date'], $_POST['activity'], $_POST['description']);
 	}
 ?>
+
 <!-- tabuka s udajmy vytiahnutimy z databazy -->
 <table class="plan">
 	<thead>
@@ -49,44 +32,34 @@
 	</thead>
 	<tbody>
 		<?php 
-		$plans = mysqli_query($con, "SELECT * FROM plan_aktivit ORDER BY date ASC");
-		while ($row = mysqli_fetch_assoc($plans)){
-			echo '<tr>';
-			echo '<td>'.$row['date'].'</td>';
-			echo '<td>'.$row['activity'].'</td>';
-			echo '<td>'.$row['description'].'</td>';
-			#echo '<td><a href="index.php??page=plan_aktivit+delete='.$row['id'].'">Zmaz</a></td>';
-			echo '<td>
-					<form method="POST">
-						<button type="submit" name="Delete" value='.$row['id'].'>Delete</button>
-						<button type="submit" name="Update_tag" value='.$row['id'].'>Update</button>
-					</form>
-				</td>';
-		}
+		$database->loadActivity();
 		?>
 	</tbody>
 </table>
+
 <!-- oznamuje co forma roby -->
-<?php 
-	if (isset($_POST['Update_tag'])){
-		echo '<h3>upravenie aktivity</h3>'; 
-	} else {
-		echo '<h3>pridanie aktivity</h3>'; 
-	}
-?>
-<!-- forma, kde sa zadavaju prikazy pre funkcie DB -->
-<form method="POST">
-	<label for="date">Tu si vyberte planovany datum aktivity</label><br>
-	<input type="date" name="date" value="2000-01-01" required><br><br>
-	<label for="activity">Zadajte meno aktivity</label><br>
-	<input type="text" name="activity" maxlength="100" required><br><br>
-	<label for="description">Zadajte popis pre aktivitu</label><br>
-	<input type="text" name="description" maxlength="1000" required><br><br>
-	<?php
-		if (!isset($_POST['Update_tag'])){
-			echo '<input type="submit" name="Add" value="pridaj aktivitu">';
+<div class="centering">
+	<?php 
+		if (isset($_POST['Update_tag'])){
+			echo '<h3>upravenie aktivity</h3>'; 
 		} else {
-			echo '<button type="submit" name="Update" value='.$_POST['Update_tag'].'>uprav aktivitu</button>';
+			echo '<h3>pridanie aktivity</h3>'; 
 		}
 	?>
-</form>
+	<!-- forma, kde sa zadavaju prikazy pre funkcie DB -->
+	<form method="POST">
+		<label for="date">Tu si vyberte planovany datum aktivity</label><br>
+		<input type="date" name="date" value="2000-01-01" required><br><br>
+		<label for="activity">Zadajte meno aktivity</label><br>
+		<input type="text" name="activity" maxlength="100" required><br><br>
+		<label for="description">Zadajte popis pre aktivitu</label><br>
+		<input type="text" name="description" maxlength="1000" required><br><br>
+		<?php
+			if (!isset($_POST['Update_tag'])){
+				echo '<input type="submit" name="Add" value="pridaj aktivitu">';
+			} else {
+				echo '<button type="submit" name="Update" value='.$_POST['Update_tag'].'>uprav aktivitu</button>';
+			}
+		?>
+	</form>
+</div>
